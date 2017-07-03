1ffd12aed0772a1f946bf14c305f099f61ae3273
ulince <4fe84.2Y>
2017-07-03 03:12:16 +0100

    Changes to harvester

--- src/harvester/osm-data.py
+++ src/harvester/osm-data.py
@@ -1,32 +1,61 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import commands
+import subprocess
 from time import time
 import sys
+import os.path
+from clint.textui import progress
+import requests
+import bz2
 
 def main(argv):
-    t0 = time()
-    print()
+
+    url = "http://download.geofabrik.de/europe/great-britain/england/greater-london-latest.osm.bz2"
+    directory = os.path.join(argv[0])
+    download_path = os.path.join(directory, "planet.osm.bz2")
+    data_path = os.path.join(directory, "planet.xml")
+    jar_path = "/home/ulincero/jars/OSM2Hive.jar"
+
+    if argv[1] == "yes":
+
+        print("Downloading planet file...")
+
+        response = requests.get(url, stream=True)
+        with open(download_path, 'wb') as f:
+            total_length = int(response.headers.get('content-length'))
+            for chunk in progress.bar(response.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+        
+        print("Decompressing file...")
+        
+        with open(data_path, 'wb') as new_file, bz2.BZ2File(download_path, 'rb') as file:
+            for data in iter(lambda : file.read(100 * 1024), b''):
+                new_file.write(data)
     
+
     print("Importing OSM data to Hive.")
     
 
     cmd = []
-    cmd.append("hive -e 'DROP TABLE IF EXISTS osmdata;'")
-    cmd.append("hive -e 'CREATE TABLE osmdata(osm_content STRING) STORED AS TEXTFILE;'")
-    cmd.append("hive -e 'LOAD DATA LOCAL INPATH \"/home/cloudera/Documents/opendata/map.xml\" OVERWRITE INTO TABLE osmdata;'")
-    cmd.append("hive -e 'DROP TABLE IF EXISTS osmnodes;'")
-    cmd.append("hive -e 'DROP TABLE IF EXISTS osmways;'")
-    cmd.append("hive -e 'DROP TABLE IF EXISTS osmrelations;'")
+    cmd.append("DROP TABLE IF EXISTS osmdata;")
+    #cmd.append("hive -e 'CREATE TABLE osmdata(osm_content STRING) STORED AS TEXTFILE;'")
+    #cmd.append("hive -e 'LOAD DATA LOCAL INPATH \"/home/cloudera/Documents/opendata/map.xml\" OVERWRITE INTO TABLE osmdata;'")
+    #cmd.append("hive -e 'DROP TABLE IF EXISTS osmnodes;'")
+    #cmd.append("hive -e 'DROP TABLE IF EXISTS osmways;'")
+    #cmd.append("hive -e 'DROP TABLE IF EXISTS osmrelations;'")
 
     for c in cmd:
         print("Executing: " + c)
-        status, output = commands.getstatusoutput(c)
-        
-        if status == 0:
+
+        try:
+            output = subprocess.check_output(["hive", "-e", "'DROP TABLE IF EXISTS osmdata;'"])
             print(output)
-        else:
-            print(output)
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+
+    exit()
 
     cmd = "hive -e 'ADD JAR /home/cloudera/jars/OSM2Hive.jar; " + \
                    "CREATE TEMPORARY FUNCTION OSMImportNodes AS \"info.pavie.osm2hive.controller.HiveNodeImporter\"; " + \
@@ -64,10 +93,6 @@ def main(argv):
         print(output)
     else:
         print(output)
-
-    elapsed = time() - t0
-    print("Total elapsed time: " + str(format(round(elapsed,3))))
-
        
 if __name__ == "__main__":
     main(sys.argv[1:])
